4dc91077c08667a6254e90957632b10a925ff743
Martin Stone <martindstone@me.com>
2020-09-23 13:02:19 -0400

    service:enable, service:disable (shortcuts to service:set)

---
+++ src/commands/service/enable.ts
@@ -0,0 +1,35 @@
+import Command from '../../base'
+import {flags} from '@oclif/command'
+// import chalk from 'chalk'
+// import cli from 'cli-ux'
+// import * as pd from '../../pd'
+import ServiceSet from './set'
+
+export default class ServiceEnable extends Command {
+  static description = 'Enable PagerDuty Services'
+
+  static flags = {
+    ...Command.flags,
+    name: flags.string({
+      char: 'n',
+      description: 'Select services whose names contain the given text',
+    }),
+    ids: flags.string({
+      char: 'i',
+      description: 'Select services with the given ID. Specify multiple times for multiple services.',
+      multiple: true,
+    }),
+  }
+
+  async run() {
+    const {flags} = this.parse(ServiceEnable)
+    let args = ['-k', 'status', '-v', 'active']
+    if (flags.name) {
+      args = [...args, '-n', flags.name]
+    }
+    if (flags.ids) {
+      args = [...args, ...flags.ids.map(e => ['-i', e]).flat()]
+    }
+    await ServiceSet.run(args)
+  }
+}
