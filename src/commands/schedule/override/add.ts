5274865e744c43357430ae1d7a20d8625dde3ffc
Martin Stone <martindstone@me.com>
2021-02-02 16:15:57 -0500

    schedule overrides

---
+++ src/commands/schedule/override/add.ts
@@ -0,0 +1,106 @@
+import Command from '../../../base'
+import {flags} from '@oclif/command'
+import cli from 'cli-ux'
+import chalk from 'chalk'
+import * as pd from '../../../pd'
+import * as utils from '../../../utils'
+import * as chrono from 'chrono-node'
+
+export default class ScheduleOverrideAdd extends Command {
+  static description = 'Add an override to a PagerDuty schedule.'
+
+  static flags = {
+    ...Command.flags,
+    id: flags.string({
+      char: 'i',
+      description: 'Add an override to the schedule with this ID.',
+      exclusive: ['name'],
+    }),
+    name: flags.string({
+      char: 'n',
+      description: 'Add an override to the schedule with this name.',
+      exclusive: ['id'],
+    }),
+    start: flags.string({
+      description: 'The start time for the override.',
+      default: 'now',
+    }),
+    end: flags.string({
+      description: 'The end time for the override.',
+      default: 'in 1 day',
+    }),
+    user_id: flags.string({
+      char: 'u',
+      description: 'The ID of the PagerDuty user for the override',
+      exclusive: ['user_email'],
+    }),
+    user_email: flags.string({
+      char: 'U',
+      description: 'The email of the PagerDuty user for the override',
+      exclusive: ['user_id'],
+    }),
+  }
+
+  async run() {
+    const {flags} = this.parse(ScheduleOverrideAdd)
+
+    // get a validated token from base class
+    const token = this.token as string
+
+    let scheduleID
+    if (flags.id) {
+      if (utils.invalidPagerDutyIDs([flags.id]).length > 0) {
+        this.error(`${chalk.bold.blue(flags.id)} is not a valid PagerDuty schedule ID`)
+      }
+      scheduleID = flags.id
+    } else if (flags.name) {
+      cli.action.start(`Finding PD schedule ${chalk.bold.blue(flags.name)}`)
+      scheduleID = await pd.scheduleIDForName(token, flags.name)
+      if (!scheduleID) {
+        cli.action.stop(chalk.bold.red('failed!'))
+        this.error(`No schedule was found with the name "${flags.name}"`, {exit: 1})
+      }
+    } else {
+      this.error('You must specify one of: -i, -n', {exit: 1})
+    }
+
+    let userID
+    if (flags.user_id) {
+      if (utils.invalidPagerDutyIDs([flags.user_id]).length > 0) {
+        this.error(`${chalk.bold.blue(flags.user_id)} is not a valid PagerDuty user ID`, {exit: 1})
+      }
+      userID = flags.user_id
+    } else if (flags.user_email) {
+      cli.action.start(`Finding PD user ${chalk.bold.blue(flags.user_email)}`)
+      userID = await pd.userIDForEmail(token, flags.user_email)
+      if (!userID) {
+        cli.action.stop(chalk.bold.red('failed!'))
+        this.error(`No user was found for the email "${flags.user_email}"`, {exit: 1})
+      }
+    } else {
+      this.error('You must specify one of: -u, -U', {exit: 1})
+    }
+
+    const body: any = {
+      override: {
+        start: chrono.parseDate(flags.start).toISOString(),
+        end: chrono.parseDate(flags.end).toISOString(),
+        user: {
+          id: userID,
+          type: 'user_reference',
+        },
+      },
+    }
+
+    cli.action.start(`Adding an override to schedule ${chalk.bold.blue(scheduleID)}`)
+    const r = await pd.request(token, `schedules/${scheduleID}/overrides`, 'POST', {}, body)
+    // const r = await pd.fetch(token, `/schedules/${scheduleID}/overrides`, params)
+    this.dieIfFailed(r)
+    const override = r.getValue()
+    if (override && override.override && override.override.id) {
+      cli.action.stop(chalk.bold.green('done'))
+      this.exit(0)
+    }
+    cli.action.stop(chalk.bold.red('failed!'))
+  }
+}
---
+++ src/commands/schedule/override/list.ts
@@ -0,0 +1,154 @@
+import Command from '../../../base'
+import {flags} from '@oclif/command'
+import cli from 'cli-ux'
+import chalk from 'chalk'
+import * as pd from '../../../pd'
+import * as utils from '../../../utils'
+import * as chrono from 'chrono-node'
+import jp from 'jsonpath'
+
+export default class ScheduleOverrideList extends Command {
+  static description = 'List a PagerDuty Schedule\'s overrides.'
+
+  static flags = {
+    ...Command.flags,
+    id: flags.string({
+      char: 'i',
+      description: 'Show overrides for the schedule with this ID.',
+      exclusive: ['name'],
+    }),
+    name: flags.string({
+      char: 'n',
+      description: 'Show overrides for the schedule with this name.',
+      exclusive: ['id'],
+    }),
+    since: flags.string({
+      description: 'The start of the date range over which you want to search.',
+      default: 'now',
+    }),
+    until: flags.string({
+      description: 'The end of the date range over which you want to search.',
+      default: 'in 30 days',
+    }),
+    keys: flags.string({
+      char: 'k',
+      description: 'Additional fields to display. Specify multiple times for multiple fields.',
+      multiple: true,
+    }),
+    json: flags.boolean({
+      char: 'j',
+      description: 'output full details as JSON',
+      exclusive: ['columns', 'filter', 'sort', 'csv', 'extended'],
+    }),
+    pipe: flags.boolean({
+      char: 'p',
+      description: 'Print override ID\'s only to stdout, for use with pipes.',
+      exclusive: ['columns', 'sort', 'csv', 'extended', 'json'],
+    }),
+    delimiter: flags.string({
+      char: 'd',
+      description: 'Delimiter for fields that have more than one value',
+      default: '\n',
+    }),
+    ...cli.table.flags(),
+  }
+
+  async run() {
+    const {flags} = this.parse(ScheduleOverrideList)
+
+    // get a validated token from base class
+    const token = this.token as string
+
+    let scheduleID
+    if (flags.id) {
+      if (utils.invalidPagerDutyIDs([flags.id]).length > 0) {
+        this.error(`${chalk.bold.blue(flags.id)} is not a valid PagerDuty schedule ID`)
+      }
+      scheduleID = flags.id
+    } else if (flags.name) {
+      cli.action.start(`Finding PD schedule ${chalk.bold.blue(flags.name)}`)
+      scheduleID = await pd.scheduleIDForName(token, flags.name)
+      if (!scheduleID) {
+        cli.action.stop(chalk.bold.red('failed!'))
+        this.error(`No schedule was found with the name "${flags.name}"`, {exit: 1})
+      }
+    } else {
+      this.error('You must specify one of: -i, -n', {exit: 1})
+    }
+
+    const params: Record<string, any> = {
+      'include[]': 'users',
+    }
+
+    if (flags.since) {
+      const since = chrono.parseDate(flags.since)
+      if (since) {
+        params.since = since.toISOString()
+      }
+    }
+    if (flags.until) {
+      const until = chrono.parseDate(flags.until)
+      if (until) {
+        params.until = until.toISOString()
+      }
+    }
+
+    cli.action.start(`Getting overrides for schedule ${chalk.bold.blue(scheduleID)}`)
+    const r = await pd.fetch(token, `/schedules/${scheduleID}/overrides`, params)
+    this.dieIfFailed(r)
+    const overrides = r.getValue()
+    if (overrides.length === 0) {
+      cli.action.stop(chalk.bold.red('none found'))
+      this.exit(0)
+    }
+    cli.action.stop(chalk.bold.green(`got ${overrides.length}`))
+
+    if (flags.json) {
+      this.log(JSON.stringify(overrides, null, 2))
+      this.exit(0)
+    }
+
+    const columns: Record<string, object> = {
+      id: {
+        header: 'ID',
+      },
+      start: {
+        get: (row: any) => row.start ? (new Date(row.start)).toLocaleString() : '',
+      },
+      end: {
+        get: (row: any) => row.end ? (new Date(row.end)).toLocaleString() : '',
+      },
+      user_id: {
+        header: 'User ID',
+        get: (row: any) => row.user.id,
+      },
+      user_name: {
+        get: (row: any) => row.user.summary,
+      },
+    }
+
+    if (flags.keys) {
+      for (const key of flags.keys) {
+        columns[key] = {
+          header: key,
+          get: (row: any) => utils.formatField(jp.query(row, key), flags.delimiter),
+        }
+      }
+    }
+
+    const options = {
+      printLine: this.log,
+      ...flags, // parsed flags
+    }
+    if (flags.pipe) {
+      for (const k of Object.keys(columns)) {
+        if (k !== 'id') {
+          const colAny = columns[k] as any
+          colAny.extended = true
+        }
+      }
+      options['no-header'] = true
+    }
+    cli.table(overrides, columns, options)
+  }
+}
