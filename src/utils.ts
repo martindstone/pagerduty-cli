e110a9b264a44d0f8bf307ae5db528a7b0d05552
Martin Stone <martindstone@me.com>
2021-02-16 17:24:10 -0500

    add raw json output shortcut for better pipe (async stdout) support

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+import cli from 'cli-ux'
+
+/* eslint-disable no-process-exit */
+/* eslint-disable unicorn/no-process-exit */
+/* eslint-disable no-console */
 export function formatField(field: any, delimiter = '\n'): string {
   switch (typeof field) {
   case 'undefined':
@@ -29,4 +34,15 @@ export function splitDedupAndFlatten(arr: any[]): string[] {
 
 export function invalidPagerDutyIDs(arr: string[]) {
   return arr.filter(e => !e.match(/^P[\w]{6}/))
+}
+
+export async function printJsonAndExit(data: any) {
+  process.stdout.on('drain', () => {
+    process.exit(0)
+  })
+  if (process.stdout.write(JSON.stringify(data, null, 2))) {
+    process.exit(0)
+  }
+  await cli.wait(10000)
+  console.error('Timed out waiting for pipe', {exit: 1})
 }
