9db0752f215e83c9663a08eb4a1cca477178f305
Martin Stone <martindstone@me.com>
2021-11-17 13:19:43 -0500

    add token flag

--- src/base.ts
+++ src/base.ts
@@ -11,38 +11,50 @@ export default abstract class Base extends AuthBase {
       char: 'b',
       description: 'Use the saved REST API token with this alias',
     }),
+    token: flags.string({
+      description: 'Ignore the saved configuration and use this token',
+      exclusive: ['useauth'],
+    }),
   }
 
   async init() {
     const {flags} = this.parse(this.ctor)
+
     this._config = new Config()
-    this.token = this._config.token(flags.useauth)
-    if (this.token) {
+    if (flags.token) {
+      const domainConfig = await Config.configForToken(flags.token)
+      this._config.put(domainConfig, true)
+      this.token = flags.token
       this.pd = new PD(this.token, flags.debug)
-    }
-    if (!this.token) {
-      if (flags.useauth) {
-        this.error(`No PagerDuty authentication was found for alias ${chalk.bold.blue(flags.useauth)}`, {
-          suggestions: [`pd auth:set -a ${flags.useauth}`, `pd login -a ${flags.useauth}`],
+    } else {
+      this.token = this._config.token(flags.useauth)
+      if (this.token) {
+        this.pd = new PD(this.token, flags.debug)
+      }
+      if (!this.token) {
+        if (flags.useauth) {
+          this.error(`No PagerDuty authentication was found for alias ${chalk.bold.blue(flags.useauth)}`, {
+            suggestions: [`pd auth:set -a ${flags.useauth}`, `pd login -a ${flags.useauth}`],
+            exit: 1,
+          })
+        }
+        this.error('No PagerDuty authentication was found', {
+          suggestions: ['pd auth:set', 'pd login'],
           exit: 1,
         })
       }
-      this.error('No PagerDuty authentication was found', {
-        suggestions: ['pd auth:set', 'pd login'],
-        exit: 1,
-      })
-    }
-    if (this._config.isOldConfig()) {
-      this._config.initEmpty()
-      if (PD.isLegacyToken(this.token)) {
-        const subdomainConfig = await Config.configForLegacyToken(this.token)
-        this._config.put(subdomainConfig, true)
-      } else if (PD.isBearerToken(this.token)) {
-        const subdomainConfig = await Config.configForBearerToken(this.token)
-        this._config.put(subdomainConfig, true)
+      if (this._config.isOldConfig()) {
+        this._config.initEmpty()
+        if (PD.isLegacyToken(this.token)) {
+          const subdomainConfig = await Config.configForLegacyToken(this.token)
+          this._config.put(subdomainConfig, true)
+        } else if (PD.isBearerToken(this.token)) {
+          const subdomainConfig = await Config.configForBearerToken(this.token)
+          this._config.put(subdomainConfig, true)
+        }
+        this._config.save()
+        this._config.load()
       }
-      this._config.save()
-      this._config.load()
     }
 
     if ((flags as any).output) {
