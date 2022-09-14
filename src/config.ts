3f5fd506561a9557b571fc7c2fbd024ca2f8c0c7
Martin Stone <martindstone@me.com>
2022-09-14 14:13:46 -0400

    remove old comments

--- src/config.ts
+++ src/config.ts
@@ -260,32 +260,3 @@ export class Config {
     return this.get()?.user?.email || ''
   }
 }
-
-// export function initConfig(): void {
-//   const configFilePath = path.join(globalAny.config.configDir, 'config.json')
-//   fs.ensureFileSync(configFilePath)
-//   fs.writeJsonSync(configFilePath, {token: null})
-// }
-
-// export function setAuth(token: string): void {
-//   const configFilePath = path.join(globalAny.config.configDir, 'config.json')
-//   let userConfig: any = null
-//   try {
-//     userConfig = fs.readJsonSync(configFilePath)
-//   } catch (error) {
-//     initConfig()
-//     userConfig = fs.readJsonSync(configFilePath)
-//   }
-//   userConfig.token = token
-//   fs.writeJsonSync(configFilePath, userConfig)
-// }
-
-// export async function promptForAuth(): Promise<string> {
-//   return cli.prompt('Enter a PD API key')
-// }
-
-// export function getAuth(): string {
-//   const configFilePath = path.join(globalAny.config.configDir, 'config.json')
-//   const userConfig = fs.readJsonSync(configFilePath)
-//   return userConfig.token
-// }
